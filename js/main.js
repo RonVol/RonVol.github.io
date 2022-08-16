7e5da173b96fb987b3a2aac7e1f818d13d580499
RonVol <rvolnets@gmail.com>
2022-08-16 12:25:47 +0300

    Inital commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function changeNav() {
+    var x = document.getElementById("topNav");
+    if (x.className === "topNav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topNav";
+    }
+  }
+
